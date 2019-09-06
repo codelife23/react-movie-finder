97db1f415aa58cc0af7c1ae8b9d35df3d6796118
Maksym Zhylenko <maksym.zhylenko@avt.pl>
2019-09-06 23:58:57 +0200

    wrap added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className="navbar navbar-dark bg-dark">
-            <div className="container">
-                <Link to="/" className="navbar-brand text-uppercase font-weight-bold">Movie Finder</Link>
+            <div className="wrap">
+                <div className="container-fluid">
+                    <Link to="/" className="navbar-brand text-uppercase font-weight-bold">Movie Finder</Link>
+                </div>
             </div>
         </nav>
     )
